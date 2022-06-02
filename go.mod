c094985e35a4446fda91157bbfebb3c0b796e2f3
dkmos2016 <dkmos2016@126.com>
2022-06-02 13:25:15 +0800

    update go.mod

---
+++ go.mod
@@ -0,0 +1,9 @@
+module godemo
+
+go 1.18
+
+require (
+	github.com/dkmos2016/go-sdk-utils v0.0.3
+	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+)
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	utils "github.com/dkmos2016/go-sdk-utils"
+	"golang.org/x/crypto/md4"
+	_ "gopkg.in/yaml.v3"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+
+	utils.Test()
+
+	//crypto
+	md4.New()
+}
